06243001c6b4796994d3aa00350111c477bf3e60
Olya Mateshov <olyamateshov@gmail.com>
2025-03-25 15:36:54 -0400

    Updated Wave 2

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -36,11 +36,42 @@ def watch_movie(user_data, title):
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
+def get_watched_avg_rating(user_data):
+
+    if len(user_data["watched"]) == 0:
+        return 0.0
+    
+    avr_rating = 0.0
+    sum_of_ratings = 0
+    for movie in user_data["watched"]:
+        sum_of_ratings += movie["rating"]
+        avr_rating = sum_of_ratings/len(user_data["watched"])
+    return avr_rating
+
+def get_most_watched_genre(user_data):
+    if not user_data:
+        return None
+    
+    freq_genre_dict = {}
+
+    for movie in user_data["watched"]: 
+        genre = movie["genre"]
+        freq_genre_dict[genre] = freq_genre_dict.get(genre, 0) + 1
+
+    most_freq_genre = ["", 0]
+
+    for key, value in freq_genre_dict.items():
+        if most_freq_genre[1] < value:
+            most_freq_genre = [key, value]
+
+    if most_freq_genre[1] == 0:
+        return None
+    else:
+        return most_freq_genre[0]
 
 # -----------------------------------------
 # ------------- WAVE 3 --------------------
 # -----------------------------------------
-
         
 # -----------------------------------------
 # ------------- WAVE 4 --------------------
