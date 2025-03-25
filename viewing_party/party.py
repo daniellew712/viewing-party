d42a7184225b02a37afc62485dba3bb84923407c
daniellew712 <xiaoyuew66@gmail.com>
2025-03-25 12:13:56 -0700

    wave 01 added for the party.py file

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,12 +1,41 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
-    pass
+
+    if not title or not genre or not rating: 
+        return None
+    new_movie = {"title": title, "genre": genre, "rating": rating}
+    return new_movie
+
+
+def add_to_watched(user_data, movie):
+    if not user_data or not movie: 
+        return None
+    user_data["watched"].append(movie)
+
+    return user_data
+
+def add_to_watchlist(user_data, movie):
+    if not user_data or not movie: 
+        return None
+    user_data["watchlist"].append(movie)
+
+    return user_data
+
+def watch_movie(user_data, title):
+
+    for movie in user_data["watchlist"]:
+        if movie["title"] == title:
+
+            user_data["watchlist"].remove(movie)
+
+            user_data["watched"].append(movie)
+
+    return user_data
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
-
 
 # -----------------------------------------
 # ------------- WAVE 3 --------------------
